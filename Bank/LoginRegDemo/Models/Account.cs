329e39f57d238cfbce639675314bcd3446d8748f
Chad Eylander <eylanderchad@gmail.com>
2018-08-21 19:15:41 -0700

    added bank

---
+++ Bank/LoginRegDemo/Models/Account.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace LoginRegDemo.Models
+{
+    public class Account
+    {
+        // Scalar Properties
+        [Key]
+        public int AccountId {get; set;}
+        public double Balance {get; set;}
+
+        public int UserId {get; set;}
+
+        public User User {get; set;}
+
+        public List<Transaction> transactions { get; set; }
+ 
+        public Account ()
+        {
+            transactions = new List<Transaction>();
+        }
+
+    }
+}
