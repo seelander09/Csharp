8beeb0bc180ec20c3b91eca861c7c50f3ff92aad
Chad Eylander <eylanderchad@gmail.com>
2018-08-28 22:36:28 -0700

    added portfolio2

---
+++ portfolio2/PortfolioTest/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PortfolioTest.Models;
+
+namespace PortfolioTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            // ViewData["Message"] = "Your contact page.";
+            return View();
+        }
+        public IActionResult Projects()
+        {
+            ViewData["Message"] = "Your contact page.";
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+    }
+}
