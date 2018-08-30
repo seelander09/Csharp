07cb4c4bac0e3ad5bde2a31b0ef6cb78b031b21f
Chad Eylander <eylanderchad@gmail.com>
2018-08-29 17:30:12 -0700

    added excercise 1

---
+++ Practice/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = -1;
+            int y = 4;
+            int z = 6;
+
+            Console.WriteLine(x + y * z);
+            Console.WriteLine((35 + 5) % 7 );
+            Console.WriteLine(14 + -4 * 6 / 11);
+            Console.WriteLine(2 + 15 / 6 * 1 - 7 % 2);
+        }
+    }
+}
