6af12f0f7d3f0f72666ac47414d5acd0a50a2df5
Chad Eylander <eylanderchad@gmail.com>
2018-08-29 18:42:55 -0700

    added multiply 3 numbers by user

---
+++ Multiply/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Multiply
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a C# Sharp program to print the output of multiplication
+            // of three numbers which will be entered by the user
+
+            int number1, number2, number3;
+
+            Console.Write("\nInput first number to multiply : ");
+            number1 = int.Parse(Console.ReadLine());
+
+            Console.Write("\nInput second number to multiply : ");
+            number2 = int.Parse(Console.ReadLine());
+
+            Console.Write("\nInput third number to multiply : ");
+            number3 = int.Parse(Console.ReadLine());
+
+            int result = number1 * number2 * number3;
+            Console.WriteLine("Output: {0} * {1} * {2} = {3}", number1, number2, number3, result);
+
+        }
+    }
+}
