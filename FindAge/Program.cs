5226e889f648c240640bf9ba78e1656255a35406
Chad Eylander <eylanderchad@gmail.com>
2018-08-30 22:26:35 -0700

    added find age

---
+++ FindAge/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FindAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+
+            Console.WriteLine("input your age please: ");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            int result = age;
+            Console.WriteLine("Are you sure ?? you look way older than {0} to me", result);
+        }
+    }
+}
