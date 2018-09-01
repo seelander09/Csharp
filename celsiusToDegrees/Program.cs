7cc663b4fa78bbf03f1f0b4ceaa1f77c36f118ca
Chad Eylander <eylanderchad@gmail.com>
2018-08-31 19:00:51 -0700

    Added temp converter

---
+++ celsiusToDegrees/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace celsiusToDegrees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the amount of celsius: ");
+            int celsius = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Kelvin = {0}", celsius + 273);
+            Console.WriteLine("Fahrenheit = {0}", celsius * 18 / 10 + 32);
+
+        }
+    }
+}
