c80582aec864e3290236892c66d8e4e8939ffd17
Chad Eylander <eylanderchad@gmail.com>
2018-09-02 21:09:01 -0700

    added negative check

---
+++ negativeCheck/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace negativeCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nEnter a number of your choice: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input second integer: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Check if one is negative and one is Positive:");
+            Console.WriteLine((x < 0 && y > 0) || (x > 0 && y < 0));
+
+        }
+    }
+}
