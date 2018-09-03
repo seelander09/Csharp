28783cfc3156bc655389b65ba094200f3b54508a
Chad Eylander <eylanderchad@gmail.com>
2018-09-02 20:25:45 -0700

    added to string front and back

---
+++ addToString/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace addToString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str; 
+            // int l= 0;
+            Console.Write("Input a string : ");
+            str = Console.ReadLine();
+            
+            if (str.Length >= 1)
+           {
+            var first = str.Substring(0,1);
+            Console.WriteLine(first + str + first);
+           }
+            }
+        }
+}
