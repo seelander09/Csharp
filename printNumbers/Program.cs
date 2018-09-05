4f9ab8eb44f7bb62935b68c7bd19274f4917b6ed
Chad Eylander <eylanderchad@gmail.com>
2018-09-05 15:00:39 -0700

    added numbers print

---
+++ printNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace printNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Odd numbers 1 to 99. Print one per line");
+
+            for (int n = 1; n < 99; n++)
+            {
+                if (n % 2 != 0)
+                {
+                    Console.WriteLine(n.ToString());
+                }
+            }
+        }
+    }
+}
