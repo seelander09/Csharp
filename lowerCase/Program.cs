f4dce69012ab58ac8ec7156f577fcce76ce056f8
Chad Eylander <eylanderchad@gmail.com>
2018-09-04 20:16:02 -0700

    added lowercase

---
+++ lowerCase/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace lowerCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = "Write a C# Program to display the following pattern using alphabet.";
+            Console.WriteLine(line.ToLower());
+
+            string upper = "ALL CAPS MAKES PEOPLE THINK YOUR YELLING";
+            Console.WriteLine(upper.ToLower());
+        }
+    }
+}
