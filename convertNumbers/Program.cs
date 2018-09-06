816d6a833ac151b53cad97f458ef08f21c472b1f
Chad Eylander <eylanderchad@gmail.com>
2018-09-06 15:56:12 -0700

    added convert numbers

---
+++ convertNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace convertNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hexval = "4B0";
+            Console.WriteLine("Hexadecimal number: " + hexval);
+
+            int decValue = int.Parse(hexval, System.Globalization.NumberStyles.HexNumber);
+            
+            Console.WriteLine("Convert to-");
+            Console.WriteLine("Decimal number: " + decValue);
+        }
+    }
+}
