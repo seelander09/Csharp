d1bcc7b24fa81d63dd1161c0b1d5d85d9c9e5ea4
Chad Eylander <eylanderchad@gmail.com>
2018-09-05 23:07:21 -0700

    added find longest word

---
+++ longestWord/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace longestWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = "Write a C# program to display the following pattern using the alphabet";
+            // makes a new array called words. the array is split into 12 strings. each word above is a string in the words array
+            string[] words = line.Split(new[] { " " }, StringSplitOptions.None);
+
+            string word = "";
+            int ctr = 0;
+            foreach (String s in words)
+            {
+                if (s.Length > ctr)
+                {
+                    word = s;
+                    ctr = s.Length;
+                }
+            }
+            Console.WriteLine(word);
+        }
+    }
+}
