87ad8ebf73d122e771105eeada16be3784d7a122
Chad Eylander <eylanderchad@gmail.com>
2018-09-08 13:28:00 -0700

    added hp lookup

---
+++ hpLookup/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Exercise36 {
+  static void Main(string[] args)
+        {
+            Console.Write("Input a first number: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input a second number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine(((m >= -10 && m <= 10)) || ((n >= -10 && n <= 10)));
+        }
+}
