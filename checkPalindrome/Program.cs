5c99167090a7b440af680deea292c98c828f1a6d
Chad Eylander <eylanderchad@gmail.com>
2018-09-09 19:59:15 -0700

    added checkPalindrome

---
+++ checkPalindrome/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace checkPalindrome
+{
+    class Program
+    {
+        public static bool checkPalindrome(string inputString)
+        {
+            char[] c = inputString.ToCharArray();
+            Array.Reverse(c);
+            return new string(c).Equals(inputString);
+        }
+        public static void Main()
+        {
+            Console.WriteLine(checkPalindrome("aaa"));
+            Console.WriteLine(checkPalindrome("abc"));
+            Console.WriteLine(checkPalindrome("madam"));
+            Console.WriteLine(checkPalindrome("1234"));
+        }
+    }
+}
