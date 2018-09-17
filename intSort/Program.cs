75b9d2798571353a9dd03f0b9945aa25c2ccc65d
Chad Eylander <eylanderchad@gmail.com>
2018-09-16 19:15:27 -0700

    added intSort

---
+++ intSort/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+
+namespace intSort
+{
+    class Program
+    {
+        public static int[] sort_numbers(int[] arra)
+        {
+
+            int[] num = arra.Where(x => x != -5).OrderBy(x => x).ToArray();
+            int ctr = 0;
+
+            return arra.Select(x => x >= 0 ? num[ctr++] : -5).ToArray();
+        }
+
+        public static void Main()
+        {
+            int[] x = sort_numbers(new int[] { -5, 236, 120, 70, -5, -5, 698, 280 });
+            foreach (var item in x)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+}
